c22b4702229094344ddb7e0dcbc7980553d42ba0
DRN88 <drn@tutanota.com>
2017-10-12 11:58:19 +0100

    Fix

--- vagrant/shellprovision.sh
+++ vagrant/shellprovision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Vagrant Shell Provision
 
-yum -y install epel-release openssh-server git ansible
+yum -y install epel-release openssh-server htop git ansible
 yum -y upgrade
 
 hostnamectl set-hostname ${1}
@@ -9,11 +9,12 @@ hostnamectl set-hostname ${1}
 cat << 'EOT' > /etc/hosts
 127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1       localhost localhost.localdomain localhost6 localhost6.localdomain6
-
 192.168.123.10 ${1}
-
 EOT
 
-git clone "https://"
-cd ansible-xrdp-gui
-ansible -i environments/local/inventory playbooks/local.yml localhost
+echo "THIS WILL TAKE A WHILE"
+echo "THIS WILL TAKE A WHILE"
+echo "THIS WILL TAKE A WHILE"
+echo "THIS WILL TAKE A WHILE"
+echo "THIS WILL TAKE A WHILE"
+sudo ansible-pull --purge --clean --force --url="https://github.com/DRN88/ansible-xrdp-gui.git" --checkout="master" --inventory="environments/local/inventory" playbooks/local.yml -v
